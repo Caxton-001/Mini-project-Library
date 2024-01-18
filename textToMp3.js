7a9ecb52b06995e47a491ccfaef1e3ca8b3303d5
Caxton-001 <mulocaxton120@gmail.com>
2024-01-18 15:28:44 +0300

    Update changes

--- textToMp3.js
+++ textToMp3.js
@@ -1,61 +1,55 @@
-#!/usr/bin/env nodejs
 /**
- * Created Caxton.
+ * Created Caxton, the dev.
  */
 
 
- var texttomp3 = require("./index");
- var fs = require('fs');
- 
- 
- 
- var text = "";
- var fn = "";
- 
- 
- 
- 
- if(process.argv.indexOf("-?")!== -1){
-   console.log("TextToMp3 bach use the TextToMp3 library which use the google translate public API to generate an mp3 with ");
-   console.log("-t \t\t\t Provide the Text here with \" around the text \", limited to 200 characters");
-   console.log("-f \t\t\t Provide the file name of MP3 you want generate, otherwise it will be generated automatically");
-   console.log("");
-   
- } 
- 
- 
- if(process.argv.indexOf("-t")!== -1)
-   text=process.argv[process.argv.indexOf("-t")+1];
- 
- if(process.argv.indexOf("-f")!== -1)
-   fn=process.argv[process.argv.indexOf("-f")+1];
- 
- text = text.replace(/ +(?= )/g,'');//remove all multiple space
- 
- if(typeof text ===  "undefined" || text === ""
-   || typeof fn === "undefined" || fn === "") { // just if I have a text I'm gonna parse
-   console.log("missing required params, check out the help with -?");
- }
- if(text.length > 200){ // check longness of text, because otherways google translate will give me a empty file
-   console.log("Text to long, split in text of 200 characters")
- }
- 
- //HERE WE GO
- options = {
-   tl: 'it'
- }
- texttomp3.getMp3(text, options, function(err, data){
-   if(err){
-     console.log(err);
-     return;
-   }
- 
-   if(fn.substring(fn.length-4, fn.length) !== ".mp3"){ // if name is not well formatted, I add the mp3 extention
-     fn+=".mp3";
-   }
-   var file = fs.createWriteStream(fn); // write it down the file
-   file.write(data);
-   file.end();
-   console.log("MP3 SAVED!");
- });
- 
+var textToMp3 = require("./index");
+var fs = require('fs');
+
+
+var text = "";
+var fn = "";
+
+if (process.argv.includes("-?")) {
+  console.log("Using the Google Translate API for text-to-speech conversion");
+  console.log("-t \t\t\t Write your text enclosed in double quotes");
+  console.log("-f \t\t\t Create a name for the file");
+  console.log("");
+  ;
+}
+
+
+if(process.argv.indexOf("-t")!== -1)
+  text=process.argv[process.argv.indexOf("-t")+1];
+
+if(process.argv.indexOf("-f")!== -1)
+  fn=process.argv[process.argv.indexOf("-f")+1];
+
+text = text.replace(/ +(?= )/g,'');
+
+if(typeof text ===  "undefined" || text === ""
+  || typeof fn === "undefined" || fn === "") {
+  console.log("Some arguments are missing.");
+}
+if(text.length > 200){ 
+  console.log("The text should not exceed 200 words.")
+}
+
+settings = {
+  tl: 'it'
+}
+
+textToMp3.getMp3(text, settings, function(err, data){
+  if(err){
+    console.log(err);
+    return;
+  }
+
+  if(fn.substring(fn.length-4, fn.length) !== ".mp3"){ 
+    fn+=".mp3";
+  }
+  var file = fs.createWriteStream(fn);
+  file.write(data);
+  file.end();
+  console.log("Text converted successfully!");
+});
